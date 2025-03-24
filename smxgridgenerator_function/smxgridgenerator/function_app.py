a01341b0033101e1b2f0c481a87a5679695e3488
Seth Harnishfeger <harnishfegerseth@gmail.com>
2025-03-23 21:23:26 -0400

    more cronjob changes

--- smxgridgenerator_function/smxgridgenerator/function_app.py
+++ smxgridgenerator_function/smxgridgenerator/function_app.py
@@ -1,8 +1,9 @@
 import logging
 import requests
 import azure.functions as func
+import azure.functions.decorators as decorators
 
-app = func.FunctionApp()
+app = decorators.FunctionApp()
 
 @app.function_name(name="smxgridgenerator")
 @app.schedule(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
@@ -14,4 +15,3 @@ def main(myTimer: func.TimerRequest) -> None:
         logging.info(f"✅ Daily grid generated: {response.json()}")
     except Exception as e:
         logging.error(f"❌ Grid generation failed: {str(e)}")
-
