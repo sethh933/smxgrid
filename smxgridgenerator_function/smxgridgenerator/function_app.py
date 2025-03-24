c0faec2e632debfa223c68d264014add3102ec1d
Seth Harnishfeger <harnishfegerseth@gmail.com>
2025-03-23 21:13:40 -0400

    cronjob changes

--- smxgridgenerator_function/smxgridgenerator/function_app.py
+++ smxgridgenerator_function/smxgridgenerator/function_app.py
@@ -1,11 +1,11 @@
-
 import logging
 import requests
 import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+@app.function_name(name="smxgridgenerator")
+@app.schedule(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def main(myTimer: func.TimerRequest) -> None:
     try:
         url = "https://smxmusegrid.azurewebsites.net/generate-and-archive-switch"
@@ -14,3 +14,4 @@ def main(myTimer: func.TimerRequest) -> None:
         logging.info(f"✅ Daily grid generated: {response.json()}")
     except Exception as e:
         logging.error(f"❌ Grid generation failed: {str(e)}")
+
